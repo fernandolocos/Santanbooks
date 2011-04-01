b2947142835382c516959dee810f6c7537ebc23a
fernandolocos@gmail.com <fernandolocos@gmail.com@54f774bf-690e-e1ef-62c6-6d52864c3734>
2011-04-01 14:24:43 +0000

    Versão bem estável

--- trunk/src/trabalho/impl/EnquirerAdvanced.java
+++ trunk/src/trabalho/impl/EnquirerAdvanced.java
@@ -1,13 +1,17 @@
 package trabalho.impl;
 
-import java.sql.*;
-import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Vector;
+
 import trabalho.banco.CriaTabela;
 import trabalho.banco.InsereTabela;
+import trabalho.impl.BaseConhecimento;
 import trabalho.inter.IBaseConhecimento;
-import trabalho.inter.IDeclaracao;
 import trabalho.inter.IEnquirer;
-import trabalho.inter.IObjetoConhecimento;
 import trabalho.inter.IResponder;
 
 public class EnquirerAdvanced implements IEnquirer 
@@ -15,7 +19,7 @@ public class EnquirerAdvanced implements IEnquirer
 	private String[] animais;
 	private String usuario, senha;
 	private Vector<String> propriedade;
-	private Vector<Integer> contaPergunta;
+	private Vector<String> listaAnimais;
 	
 	public EnquirerAdvanced()
 	{
@@ -26,12 +30,16 @@ public class EnquirerAdvanced implements IEnquirer
 		IBaseConhecimento bc = new BaseConhecimento();
 		animais = bc.listaNomes();
 		propriedade = new Vector<String>();
+		listaAnimais = new Vector<String>();
 		
-		// criando uma tabela no banco de dados	
+		//insere elementos da lista de nomes em um Vector
+		for(int i = 0; i < animais.length; i++)
+			listaAnimais.add(animais[i]);
+		
+		// criando um banco de dados e uma tabela	
 		new CriaTabela(usuario, senha);
-		// inserindo os dados nas tabelas
+		// insere os dados na tabela
 		new InsereTabela(animais, usuario, senha);
-		
 	}
 	
 	/**
@@ -42,18 +50,19 @@ public class EnquirerAdvanced implements IEnquirer
 	 */
 	public String proximaPergunta (Vector<String> propriedades)
 	{
-		String pergunta = new String();
+		/*String pergunta = new String();
 		for(int i = 0; i < propriedades.size(); i++)
 		{
 			//if(propriedades.contains(decl.getPropriedade())
 		}
-		return pergunta;
+		return pergunta;*/
+		return propriedades.firstElement();
 	}
 	
 	public void connect(IResponder responder) 
 	{    
 		try 
-        {
+        {		
         	//Carrega driver do MySQL  
 	        Class.forName("com.mysql.jdbc.Driver");  
 	        //Abre conexao com o banco  
@@ -61,18 +70,57 @@ public class EnquirerAdvanced implements IEnquirer
 	        // cria um objeto de comandos SQL para a base
 	        Statement stmt = conn.createStatement();
 	        
-	        // aciona metodo que executa comando SQL
-            ResultSet resultado = stmt.executeQuery("SELECT PERGUNTA FROM GERAL");
-
-            // lista o conteudo da tabela no console
-            boolean temConteudo = resultado.next();
-            while (temConteudo)
-            {
-                propriedade.add(resultado.getString("PERGUNTA"));
-                temConteudo = resultado.next();
-            }
-            
-            String pergunta = proximaPergunta(propriedade);
+	        // enquanto nao encontrar o animal procurado
+	        while(listaAnimais.size() > 1)
+	        {
+	        	// limpa os dados dos vetores auxiliares para as novas consultas
+	        	if(propriedade.size() > 0) propriedade.removeAllElements();
+	        	if(listaAnimais.size() > 0)listaAnimais.removeAllElements();
+	        	
+		        /* faz uma consulta que retorna todas as perguntas que ainda nao foram feitas e existem nos animais
+		         que ainda restam como possibilidade de ser o animal procurado*/
+	            ResultSet listaPerguntas = stmt.executeQuery("SELECT PERGUNTA FROM BASE.GERAL G " +
+	            		"WHERE G.`CHECKED` = 1 ORDER BY G.`PERGUNTA`");
+	
+	            // insere o retorno da consulta em um vetor de String
+	            boolean temConteudo = listaPerguntas.next();
+	            while (temConteudo)
+	            {
+	                propriedade.add(listaPerguntas.getString("PERGUNTA"));
+	                temConteudo = listaPerguntas.next();
+	            }
+	            
+	            System.out.println(propriedade.toString());
+	            
+	            // chama o metodo que retorna a proxima pergunta
+	            String pergunta = proximaPergunta(propriedade);
+	            // chama o responder
+	            String resposta = responder.ask(pergunta);
+	            
+	            // faz uma consulta que retorna quais animais ainda podem ser o animal procurado
+	            ResultSet listaAnimaisPossiveis = stmt.executeQuery("SELECT G.`ANIMAIS` FROM BASE.GERAL G " +
+	            		"WHERE G.`PERGUNTA` = '"+pergunta+"' AND G.`RESPOSTA` = '"+resposta+"'");
+	            
+	            // insere o retorno da consulta em um vetor de String
+	            temConteudo = listaAnimaisPossiveis.next();
+	            while (temConteudo)
+	            {
+	            	listaAnimais.add(listaAnimaisPossiveis.getString("ANIMAIS"));
+	                temConteudo = listaAnimaisPossiveis.next();
+	            }
+	            
+	            // seta o valor FALSE na coluna CHECK, para os animais e perguntas ja eliminadas
+	            stmt.executeUpdate("UPDATE GERAL SET CHECKED=0");
+	            for(int i = 0; i < listaAnimais.size(); i++)
+	            {
+	            	stmt.executeUpdate("UPDATE GERAL SET CHECKED=1 WHERE ANIMAIS = '"+listaAnimais.get(i)+
+	            			"' AND PERGUNTA != '"+pergunta+"'");
+	            	listaAnimais.iterator();
+	            }
+	        }
+	        
+            System.out.println(listaAnimais.toString());
+            responder.finalAnswer(listaAnimais.toString());
             
             // fecha stmt
             stmt.close();
