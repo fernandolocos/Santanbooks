d47dd6cedd67384f5cefb008b3c8fc73a86174ae
jaum.scalett@gmail.com <jaum.scalett@gmail.com@54f774bf-690e-e1ef-62c6-6d52864c3734>
2011-04-04 04:43:00 +0000

    EnquirerAdvanced com atualizações

--- src/trabalho/teste/AppTrabalho.java
+++ src/trabalho/teste/AppTrabalho.java
@@ -18,15 +18,15 @@ public class AppTrabalho
 		
 		for(int i = 0; i < animais.length; i++){
 			
-			System.out.println("Animal pensado: "+animais[i]);
+			System.out.println("Animal pensado: "+animais[i]);//System.out.println("Animal pensado: tigre-de-bengala");
 			System.out.print("EnquirerBasic: ");
 			IEnquirer e = new EnquirerBasic();
-			IResponder r = new Responder(animais[i]);
+			IResponder r = new Responder(animais[i]);//IResponder r = new Responder("tigre-de-bengala");
 			e.connect(r);
 			
 			System.out.print("EnquirerAdvanced: ");
 			IEnquirer ea = new EnquirerAdvanced();
-			IResponder ra = new Responder(animais[i]);
+			IResponder ra = new Responder(animais[i]);//IResponder ra = new Responder("tigre-de-bengala");
 			ea.connect(ra);
 			System.out.print("\n");
 		}
