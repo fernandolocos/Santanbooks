431350f9fe23be688c0f7397d1fe190f2c254432
fernandolocos@gmail.com <fernandolocos@gmail.com@54f774bf-690e-e1ef-62c6-6d52864c3734>
2011-04-29 21:32:59 +0000

    alterações no componente

--- src/pt/c03ensaios/fejao/IPossibleAnimalsHash.java
+++ src/pt/c03ensaios/fejao/IPossibleAnimalsHash.java
@@ -12,9 +12,27 @@ import anima.component.ISupports;
 @ComponentInterface("<http://purl.org/dcc/pt.c03ensaios.fejao.IPossibleAnimals>")
 public interface IPossibleAnimalsHash extends ISupports{
 
+	 /**
+     * Put a list of possible animals in the hash 
+     * @param animals
+     */
 	public void putPossibleAnimalsList(List<String> animals);
 	
+	 /**
+     * Get a list of possible animals in the hash 
+     * @return List<String> list
+     */
 	public List<String> getPossibleAnimalsList();
 	
+	 /**
+     * Get the list size 
+     * @return int size
+     */
 	public int getListSize();
+	
+	 /**
+     * Determines possible animals from the response to a new question
+     * @param question, answer
+     */
+	public void DeterminesPossibleAnimals(String question, String answer);
 }
--- src/pt/c03ensaios/fejao/PossibleAnimalsHash.java
+++ src/pt/c03ensaios/fejao/PossibleAnimalsHash.java
@@ -1,14 +1,46 @@
 package pt.c03ensaios.fejao;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import pt.c03ensaios.frango.IQuestionsHash;
+import pt.c03ensaios.frango.QuestionsHash;
 
 import anima.annotation.Component;
 import anima.component.base.ComponentBase;
+import anima.factory.IGlobalFactory;
+import anima.factory.context.componentContext.ComponentContextFactory;
 
 @Component(id = "<http://purl.org/dcc/pt.c03ensaios.fejao.PossibleAnimalsHash>", 
 		provides = { "<http://purl.org/dcc/pt.c03ensaiosfoundations.fejao.IPossibleAnimalsHash>" })
 public class PossibleAnimalsHash extends ComponentBase implements IPossibleAnimalsHash {
 	private List<String> animals = null;
+	private IQuestionsHash hashAnswerYes;
+	private IQuestionsHash hashAnswerNo;
+	private IQuestionsHash hashAnswerDontKnow;
+	
+	public PossibleAnimalsHash(){
+		try {
+			// creates a global factory
+			IGlobalFactory factory = ComponentContextFactory
+					.createGlobalFactory();
+
+			factory.registerPrototype(QuestionsHash.class);
+
+			// instances the component based on its URI
+			hashAnswerYes = factory
+					.createInstance("<http://purl.org/dcc/pt.c03ensaios.frango.QuestionsHash>");
+
+			hashAnswerNo = factory
+					.createInstance("<http://purl.org/dcc/pt.c03ensaios.frango.QuestionsHash>");
+
+			hashAnswerDontKnow = factory
+					.createInstance("<http://purl.org/dcc/pt.c03ensaios.frango.QuestionsHash>");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+	}
 	
 	public List<String> getPossibleAnimalsList() {
 		return animals;
@@ -23,5 +55,44 @@ public class PossibleAnimalsHash extends ComponentBase implements IPossibleAnima
 		if(animals != null)
 			tamanho = animals.size();
 		return tamanho;
+	}
+
+	public void DeterminesPossibleAnimals(String question, String answer) {		
+		if (answer.equalsIgnoreCase("sim")) {				
+				animals = mergeList(hashAnswerYes.getAnimals(question), animals);				
+		} else if (answer.equalsIgnoreCase("nao")) {
+				animals = mergeList(hashAnswerNo.getAnimals(question), animals);
+		} else if (answer.equalsIgnoreCase("nao sei")) {
+				animals = mergeList(hashAnswerDontKnow.getAnimals(question), animals);
+		}
+	}
+	
+	private List<String> mergeList(List<String> l1, List<String> l2){
+		List<String> mergeList = new ArrayList<String>();
+		int size;
+		
+		if (l1.isEmpty()) {
+			mergeList = l2;
+		} else if (l2.isEmpty()) {
+			mergeList = l1;
+		} else {
+			if(l1.size() > l2.size())
+				size = l1.size();
+			else
+				size = l2.size();
+	
+			for (int i = 0; i < size; i++) {
+				if (l1.size() > l2.size()) {
+					if (l2.contains(l1.get(i))) {
+						mergeList.add(l1.get(i));
+					}
+				} else {
+					if (l1.contains(l2.get(i))) {
+						mergeList.add(l2.get(i));
+					}
+				}
+			}
+		}
+		return mergeList;
 	}
 }
