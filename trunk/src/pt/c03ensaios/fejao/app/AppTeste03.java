6aa293cdaf3f7e8053a51a66b59bad98780ddb01
fernandolocos@gmail.com <fernandolocos@gmail.com@54f774bf-690e-e1ef-62c6-6d52864c3734>
2011-05-14 14:28:31 +0000

    versão final do pacote fejao

--- trunk/src/pt/c03ensaios/fejao/app/AppTeste03.java
+++ trunk/src/pt/c03ensaios/fejao/app/AppTeste03.java
@@ -4,10 +4,9 @@ import pt.c01interfaces.s01chaveid.s01base.impl.BaseConhecimento;
 import pt.c01interfaces.s01chaveid.s01base.inter.IBaseConhecimento;
 import pt.c01interfaces.s01chaveid.s01base.inter.IEnquirer;
 import pt.c01interfaces.s01chaveid.s01base.inter.IResponder;
-import pt.c01interfaces.s01chaveid.s02exemplo.impl.Responder;
+import pt.c03ensaios.frango.app.Responder;
 
-
-public class AppTeste01 {
+public class AppTeste03 {
 
 	public static void main(String[] args) {
 
@@ -23,6 +22,13 @@ public class AppTeste01 {
             eb.connect(ra);
         }
         
+        try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
         for (int i = 0; i < listaNomes.length; i++) {
             System.out.print("Advanced: ");
             ra = new Responder(listaNomes[i]);
